864fe84669261485b6d873785f6e2021b5b10854
oswystk15662 <oswystk15662@keio.jp>
2026-08-12 06:27:28 +0900

    Enable GLIM and update object tracking

--- src/robot/launch/jetson_bringup.launch.py
+++ src/robot/launch/jetson_bringup.launch.py
@@ -221,7 +221,7 @@ def generate_launch_description():
             DeclareLaunchArgument("enable_zed2i", default_value="true"),
             DeclareLaunchArgument(
                 "enable_glim",
-                default_value="false",
+                default_value="true",
                 description="Load GLIM into the Livox component container",
             ),
             DeclareLaunchArgument(
